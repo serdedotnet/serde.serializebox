00a270b54264d1a9b42fae042d126030eb3cd9aa
Andy Gocke <andy@commentout.net>
2025-04-15 23:23:35 -0700

    Add test

---
+++ test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Serde.Json;
+
+namespace Serde.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var list = new List<string> { "a", "b", "c" };
+        var box = SerializeBox.Create(list);
+        var result = JsonSerializer.Serialize(box, box);
+        Assert.Equal("""
+        ["a","b","c"]
+        """, result);
+
+    }
+}
